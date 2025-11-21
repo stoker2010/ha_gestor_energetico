6081e4816099c7318a84a35a281ed53f152fd840
Alberto Domínguez <84203914+stoker2010@users.noreply.github.com>
2025-11-21 12:23:28 +0100

    Create const.py

---
+++ custom_components/ha_gestor_energetico/const.py
@@ -0,0 +1,17 @@
+"""Constantes para Gestor Energético."""
+
+DOMAIN = "ha_gestor_energetico"
+CONF_GRID_SENSOR = "grid_sensor"
+CONF_PROD_SENSOR = "production_sensor"
+CONF_POWER_VALLE = "power_valle"
+CONF_POWER_PUNTA = "power_punta"
+
+# Tarifas
+TARIFF_PUNTA = "Punta"
+TARIFF_LLANO = "Llano"
+TARIFF_VALLE = "Valle"
+
+# Periodos (P1, P2, P3)
+PERIOD_P1 = "P1" # Punta
+PERIOD_P2 = "P2" # Llano
+PERIOD_P3 = "P3" # Valle
